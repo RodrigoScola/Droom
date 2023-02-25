635cc287324456ed459573540756e8de29609ae8
Snuffy <rodrigo.sgarabotto.scola@gmail.com>
2023-02-24 21:09:30 -0300

    env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: "droom---test.firebaseapp.com",
-	databaseURL: "https://droom---test-default-rtdb.firebaseio.com",
-	projectId: "droom---test",
-	storageBucket: "droom---test.appspot.com",
-	messagingSenderId: "1098064259229",
-	appId: "1:1098064259229:web:8bba9447176c6a3d044c74",
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = getAuth();
